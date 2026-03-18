c1fe114ae5dede2b30cf9d952c141064dc8cd965
German Blanco <germanblanco@chaye.com.ar>
2026-03-18 14:40:10 -0300

    chore: update prod API base URL and ignore VS files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appUrl: 'https://mimir.chayehnos.local',
-  apiBaseUrl: 'http://192.168.1.115',
+  apiBaseUrl: 'http://192.168.1.116',
   apiPrefix: '/api/v1',
 };
